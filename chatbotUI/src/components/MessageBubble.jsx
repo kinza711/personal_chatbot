bc948fce9fc47e5d8d9848999439395791e9045b
kinza <kinzashahzadi7181@gmail.com>
2026-04-05 18:11:27 +0500

    feat: cahtbot Ui fixes

--- chatbotUI/src/components/MessageBubble.jsx
+++ chatbotUI/src/components/MessageBubble.jsx
@@ -3,7 +3,7 @@ export default function MessageBubble({ message }) {
   return (
     <div className={`flex flex-col ${isUser ? "items-end" : "items-start"}`}>
       <div
-        className={`max-w-[85%] p-3 rounded-2xl shadow-sm text-sm ${
+        className={`max-w-[85%]  p-3 rounded-2xl shadow-sm text-sm ${
           isUser
             ? "bg-[#3bb6c9] text-white rounded-tr-none shadow-md"
             : "bg-white text-slate-700 rounded-tl-none border border-slate-100"
